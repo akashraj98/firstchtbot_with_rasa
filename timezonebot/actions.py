908d849c0918045c4b5b3384fa1cad78ef25ab46
Akash raj7 <akashraj7713@gmail.com>
2020-08-04 11:01:08 +0000

    Added actions and exposed endpoint

--- timezonebot/actions.py
+++ timezonebot/actions.py
@@ -7,21 +7,34 @@
 
 # This is a simple example for a custom action which utters "Hello World!"
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+from typing import Any, Text, Dict, List
+
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+
+timezones = {
+    "London": "UTC+1:00",
+    "Sofia" : "UTC+3:00",
+    "Lisbon": "UTC+1:00",
+    "Mumbai": "UTC+5:30"
+}
+class ActionShowTimeZone(Action):
+
+    def name(self) -> Text:
+        return "action_show_time_zone"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        city = tracker.get_slot("city")
+        timezone = timezones.get(city)
+
+        if timezone is None:
+            output = "Could not find the time zone of {}".format(city)
+        else:
+            output = "Time zone of {} is {}".format(city,timezone)
+
+        dispatcher.utter_message(text=output)
+
+        return []
--- timezonebot/data/stories.md
+++ timezonebot/data/stories.md
@@ -9,15 +9,17 @@
   - utter_ask_location
 * city_info
   - utter_finding_time_zone
+  - action_show_time_zone
 * thanks
   - utter_you_are_welcome
   - utter_goodbye
 
-## ask for time zone long
+## ask for time zone short
 * greet
   - utter_greet
-* find_time_zone
-  - utter_ask_location
+* find_time_zone_for_location
+  - utter_finding_time_zone
+  - action_show_time_zone
 * thanks
   - utter_you_are_welcome
   - utter_goodbye
